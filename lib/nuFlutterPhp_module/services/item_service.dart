66089b80f5a7908fd4728a39c52a7507a2753f15
unvireak <dydy11v@gmail.com>
2024-06-05 09:04:52 +0700

    note

--- lib/nuFlutterPhp_module/services/item_service.dart
+++ lib/nuFlutterPhp_module/services/item_service.dart
@@ -6,7 +6,9 @@ import '../models/item_model.dart';
    
     class ItemService{
   static Future<ItemModel> readAPI() async {
+    // Android simulator
     String url = 'http://10.0.2.2:8090/nuFlutterPhp/product.php?key=d033e22ae348aeb5660fc2140aec35850c4da997';
+    // iOS simutalor
     // String url = 'http://localhost:8090/nuFlutterPhp/product.php?key=d033e22ae348aeb5660fc2140aec35850c4da997';
 
     try {
